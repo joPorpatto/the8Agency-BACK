e9779327c59391d7578050143d6fbe044a639e6b
joPorpatto <joel.porpatto@gmail.com>
2022-04-25 01:42:38 -0300

    cors added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { dbConnection } = require('./database/config');
 require('dotenv').config();
+const { dbConnection } = require('./database/config');
+const cors = require('cors');
 
 const app = express()
 app.use(express.static('public'))
 dbConnection()
+app.use(cors());
 app.use(express.json() );
 app.use('/api/form', require('./routes/form'));
 app.listen(process.env.PORT, ( ) =>{
